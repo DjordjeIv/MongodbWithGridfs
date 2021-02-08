a0b885dc5982bc1655ac14b13e1e5c74c595d856
DjordjeIv <78754956+DjordjeIv@users.noreply.github.com>
2021-02-08 19:24:08 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ProbaZaMongoDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Upload = new System.Windows.Forms.Button();
+            this.Download = new System.Windows.Forms.Button();
+            this.btnBrows = new System.Windows.Forms.Button();
+            this.textPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Upload
+            // 
+            this.Upload.Location = new System.Drawing.Point(12, 12);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(93, 23);
+            this.Upload.TabIndex = 0;
+            this.Upload.Text = "Upload";
+            this.Upload.UseVisualStyleBackColor = true;
+            this.Upload.Click += new System.EventHandler(this.Upload_Click);
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(12, 43);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(93, 23);
+            this.Download.TabIndex = 1;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.Download_Click);
+            // 
+            // btnBrows
+            // 
+            this.btnBrows.Location = new System.Drawing.Point(112, 11);
+            this.btnBrows.Name = "btnBrows";
+            this.btnBrows.Size = new System.Drawing.Size(75, 23);
+            this.btnBrows.TabIndex = 2;
+            this.btnBrows.Text = "Brows";
+            this.btnBrows.UseVisualStyleBackColor = true;
+            this.btnBrows.Click += new System.EventHandler(this.btnBrows_Click);
+            // 
+            // textPath
+            // 
+            this.textPath.Location = new System.Drawing.Point(477, 12);
+            this.textPath.Name = "textPath";
+            this.textPath.ReadOnly = true;
+            this.textPath.Size = new System.Drawing.Size(288, 22);
+            this.textPath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path:";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(193, 11);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(231, 22);
+            this.textName.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(477, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(288, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Path:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "To:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPath);
+            this.Controls.Add(this.btnBrows);
+            this.Controls.Add(this.Download);
+            this.Controls.Add(this.Upload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Upload;
+        private System.Windows.Forms.Button Download;
+        private System.Windows.Forms.Button btnBrows;
+        private System.Windows.Forms.TextBox textPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using System.IO;
+using System.Configuration;
+using System.Windows.Forms.VisualStyles;
+using System.Runtime.CompilerServices;
+
+namespace ProbaZaMongoDB
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Upload_Click(object sender, EventArgs e)
+        {
+            if(textPath.Text == "")
+            {
+                MessageBox.Show("Please select a path!");
+            }
+            var connectionString = "mongodb://localhost/?safe=true";
+            MongoClient client = new MongoClient(connectionString);
+            MongoServer server = client.GetServer();
+            var database = client.GetDatabase("Proba");
+
+
+            var bucket = new GridFSBucket(database);
+            string filename = textName.Text;
+            FileStream source = new FileStream(textPath.Text , FileMode.Open);
+/*            MemoryStream source = new MemoryStream(Encoding.UTF8.GetBytes(@textPath.Text));*/
+            var id = bucket.UploadFromStream(filename, source);
+        }
+
+        private void Download_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            MongoClient client = new MongoClient(connectionString);
+            MongoServer server = client.GetServer();
+            var database = client.GetDatabase("Proba");
+
+            var bucket = new GridFSBucket(database);
+
+            string filename = "Dnd uputstvo za mene.odt";
+
+            string where = ("D:\\MongoProba\\" + filename);
+
+            using (var newFs = new FileStream(where, FileMode.Create))
+            {
+                var t1 = bucket.DownloadToStreamByNameAsync(filename, newFs);
+                Task.WaitAll(t1);
+                newFs.Flush();
+                newFs.Close();
+            }
+        }
+
+        private void btnBrows_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    textPath.Text = ofd.FileName;
+                    textName.Text = ofd.SafeFileName;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
